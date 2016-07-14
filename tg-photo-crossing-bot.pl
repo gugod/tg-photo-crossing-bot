565248c94016078d26cf8e950453de4af65da2d6
Kang-min Liu <gugod@gugod.org>
2016-07-14 23:38:53 +0200

    fix a4040b950f2a2432ddca0109cc137499ad597513

--- tg-photo-crossing-bot.pl
+++ tg-photo-crossing-bot.pl
@@ -32,7 +32,7 @@ sub db_find_or_create_chat_id {
         ($id) = $dbh->selectrow_array("SELECT max(id) FROM chats");
         $id //= 0;
         $id +=  1;
-        $dbh->do("INSERT INTO chats(`id`, `tg_chat_id`) VALUES (?,?), undef, $id, $tg_chat_id");
+        $dbh->do("INSERT INTO chats(`id`, `tg_chat_id`) VALUES (?,?)", {}, $id, $tg_chat_id);
     }
     return $id;
 }
@@ -40,7 +40,7 @@ sub db_find_or_create_chat_id {
 sub db_find_photo_id {
     my ($tg_file_id) = @_;
     my $dbh = dbh_rw();
-    my ($id) = $dbh->selectrow_array("SELECT id FROM photos WHERE tg_file_id = ?", undef, $tg_file_id);
+    my ($id) = $dbh->selectrow_array("SELECT id FROM photos WHERE tg_file_id = ?", {}, $tg_file_id);
     return $id;
 }
 
@@ -52,7 +52,7 @@ sub db_insert_photo {
         ($id) = $dbh->selectrow_array("SELECT max(id) FROM photos");
         $id //= 0;
         $id +=  1;
-        $dbh->do("INSERT INTO photos(`id`, `tg_file_id`) VALUES (?,?), {}, $id, $tg_file_id");
+        $dbh->do("INSERT INTO photos(`id`, `tg_file_id`) VALUES (?,?)", {}, $id, $tg_file_id);
     }
     return $id;
 }
@@ -66,7 +66,13 @@ sub db_store_photos_sent {
 sub db_store_photos_received {
     my ($chat_id, $photo_id) = @_;
     my $dbh = dbh_rw();
-    $dbh->do("INSERT INTO photos_received (`chat_id`, `photo_id`) VALUES (?,?)", undef, $chat_id, $photo_id);
+    $dbh->do("INSERT INTO photos_received (`chat_id`, `photo_id`) VALUES (?,?)", {}, $chat_id, $photo_id);
+}
+
+sub db_get_tg_file_id {
+    my ($photo_id) = @_;
+    my ($tg_file_id) = dbh_rw()->selectrow_array("SELECT tg_file_id FROM photos WHERE id = ?", {}, $photo_id);
+    return $tg_file_id;
 }
 
 sub db_get_unseen_photo {
@@ -76,7 +82,7 @@ sub db_get_unseen_photo {
     my ($max_photo_id) = $dbh->selectrow_array("SELECT max(id) FROM photos");
     my $seen = 1;
     my $count = 0;
-    while($count++ < 1000 && (!$photo_id || $seen)) {
+    while($count++ < 2 && (!$photo_id || $seen)) {
         my $o = int rand($max_photo_id);
         ($photo_id) = $dbh->selectrow_array("SELECT `id` FROM photos WHERE id = ?", undef, $o);
         next unless $photo_id;
@@ -196,27 +202,25 @@ sub tg_init {
     my ($token) = @_;
     $CONTEXT->{tg_token} = $token;
 
-    my $tgbot = WWW::Telegram::BotAPI->new( token => $token );
+    my $tgbot = WWW::Telegram::BotAPI->new( token => $token, async => 1 );
 
-    # my $get_me_cb;
-    # $get_me_cb = sub  {
-    #     my ($ua, $tx) = @_;
-    #     say "Hey";
-    #     if ($tx->success) {
-    #         my $r = $tx->res->json;
-    #         Mojo::Util::dumper(['getMe', $r]);
-    #         tg_get_updates();
-    #         Mojo::IOLoop->recurring( 5, \&tg_get_updates );
-    #     } else {
-    #         Mojo::Util::dumper(['getMe Failed.', $tx->res->body]);
-    #         Mojo::IOLoop->timer( 5 => sub { $tgbot->api_request(getMe => $get_me_cb) });
-    #     } 
-    # };
+    my $get_me_cb;
+    $get_me_cb = sub  {
+        my ($ua, $tx) = @_;
+        say "Hey";
+        if ($tx->success) {
+            my $r = $tx->res->json;
+            Mojo::Util::dumper(['getMe', $r]);
+	    tg_get_updates();
+            Mojo::IOLoop->recurring( 5, \&tg_get_updates );
+        } else {
+            Mojo::Util::dumper(['getMe Failed.', $tx->res->body]);
+            Mojo::IOLoop->timer( 5 => sub { $tgbot->api_request(getMe => $get_me_cb) });
+        } 
+    };
 
-    # Mojo::IOLoop->timer( 1 => sub { $tgbot->api_request(getMe => $get_me_cb) });
-
-    tg_get_updates();
-    Mojo::IOLoop->recurring( 15, \&tg_get_updates );
+    Mojo::IOLoop->timer( 15 => sub { $tgbot->api_request(getMe => $get_me_cb) });
+    $CONTEXT->{tg_bot} = $tgbot;
     return $tgbot;
 }
 
@@ -225,7 +229,8 @@ sub MAIN {
     die("Missing mandatory parameter: --db") unless -f $args{db};
 
     $CONTEXT->{db} = $args{db};
-    $CONTEXT->{tg_bot} = tg_init( $args{telegram_token} );
+
+    tg_init( $args{telegram_token} );
 
     Mojo::IOLoop->start;
 }
